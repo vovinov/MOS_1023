48d5d46c84ed5e40364e8707dc5f9e701b205cf1
vovinov <v.v.ovinov@yandex.ru>
2023-11-08 03:11:43 +0300

    exercise 1.6 solution

--- lab1.cpp
+++ lab1.cpp
@@ -11,19 +11,21 @@ int main()
 {
 
     DWORD size = 256;
-    PVOID wheel_scroll_chars {};
+    PVOID wheel_scroll_chars;
     char buffer[256];
     char username[256];
     TCHAR windows_path_buffer[256];
     TCHAR system_path_buffer[256];
     TCHAR temp_path_buffer[256];
     TCHAR wallpaper_buffer[256];
+    DWORD context_menu_buffer[256];
+    UINT mouse_buffer[256];
+    DWORD system_color_highlight;
 
     OSVERSIONINFO os;
 
     #define DIRECTORY_PATH 32767
     #define WALLPAPER_PATH 32767
-
 
     // void PrintError(LPCTSTR errDesc);
 
@@ -74,9 +76,49 @@ int main()
     SystemParametersInfoA(SPI_GETDESKWALLPAPER, WALLPAPER_PATH, wallpaper_buffer, 0);
     std::cout << "System parameters. Path to wallpaper: " << wallpaper_buffer << std::endl;
     
-    // Получаем и выводим в консоль значение времени ожидания заставки экрана (в секундах)
-    SystemParametersInfoA(SPI_GETWHEELSCROLLCHARS, 0, wheel_scroll_chars, 0);
-    std::cout << "System parameters. Screensaver timeout: " << wheel_scroll_chars << std::endl;
+    // Получаем и выводим в консоль значение времени ожидания контекстного меню (в милисекундах)
+    SystemParametersInfoA(SPI_GETMENUSHOWDELAY, 0, context_menu_buffer, 0);
+    std::cout << "System parameters. Mouseover context menu activate after " <<  *context_menu_buffer << " ms" << std::endl;
+
+    // Получаем и выводим в консоль текущее значение скорость мыши (значения от 1 до 20, по умолчанию - 10)
+    SystemParametersInfoA(SPI_GETMOUSESPEED, 0, mouse_buffer, 0);
+    std::cout << "System parameters. Mouse buffer: " <<  *mouse_buffer << std::endl;
+
+    //  Системные цвета
+    
+    int aElements[1] = {COLOR_HIGHLIGHT};
+    DWORD aOldColors[1];
+    DWORD aNewColors[1];
+
+    // Получение цвета для рамки выделения файлов
+ 
+    aOldColors[0] = GetSysColor(aElements[0]); 
+
+    printf("Current highlight color: {0x%x, 0x%x, 0x%x}\n", 
+        GetRValue(aOldColors[0]), 
+        GetGValue(aOldColors[0]), 
+        GetBValue(aOldColors[0]));
+
+    // Установка нового цвета
+    aNewColors[0] = RGB(0xFF, 0x7F, 0x50);  // оранженый
+
+    printf("\nNew highlight color: {0x%x, 0x%x, 0x%x}\n", 
+        GetRValue(aNewColors[0]), 
+        GetGValue(aNewColors[0]), 
+        GetBValue(aNewColors[0]));
+
+    SetSysColors(1, aElements, aNewColors); 
+
+    printf("\nHighlight color have been changed.\n");
+    printf("Reverting to previous colors in 30 seconds...\n");
+
+    Sleep(30000);    
+
+    // Возврат к первоначальным настройкам цвета
+
+    SetSysColors(1, aElements, aOldColors); 
+
+    
 
     return 0;
 }
