308dffaca6c55d0889abbfa18e5d1fb2c1c65359
vovinov <v.v.ovinov@yandex.ru>
2023-11-21 22:22:58 +0300

    Lab 3 progress - 21.11.23

--- lab3.1.cpp
+++ lab3.1.cpp
@@ -14,12 +14,35 @@
 #include <string>
 #include <filesystem>
 #include <windows.h>
+#include <thread>
 
+// Пример использования синхронизации с помощью метода критической секции
+
+CRITICAL_SECTION critical_section;
+
+void print_thread(int k) {
+  EnterCriticalSection(&critical_section);
+
+  for (int i = 0; i < 10; i++) {
+  std::cout << k;
+
+  Sleep(50);
+
+  LeaveCriticalSection(&critical_section);
+};
 
 int main() {
-  // Пример использования синхронизации с помощью метода критической секции
-  CRITICAL_SECTION critical_section;
+  
   InitializeCriticalSection(&critical_section);
   
+  HANDLE hTread;
+  DWORD IDThread;
+
+  thread thread1(print_thread ,1);
+
+  DeleteCriticalSection(&critical_section);
+
+  pause(0)
+
   return 0;
 }
