86a42d64fade86b73b554198eba6fa14e27d0240
vovinov <v.v.ovinov@yandex.ru>
2023-12-04 17:54:41 +0300

    Lab2 text solution

--- lab2.cpp
+++ lab2.cpp
@@ -1,40 +1,4 @@
-/*
-2. Программа, которая принимая дескриптор, имя или полное имя модуля, возвращает
-другие два элемента в своих выходных параметрах (выполнить задание для своей
-программы и для любой известной библиотеки). 
-Например:
-• Получить дескриптор, имя и полное имя ЕХЕ.
-Handle: &HFFFFFFFF
-Name: VB6.EXE
-FName: G:\Visual Studio\VB98\VB6.EXE
-• Получить имя и полное имя модуля.
-Задаем: Handle: &H77E70000
-Получаем:
-Name: User32.dll
-FName: G:\WINNT\system32\USER32.DLL
-• Получить дескриптор и полное имя.
-Задаем: Name: User32.dll
-Получаем:
-Handle: &H77E70000
-FName: G:\WINNT\system32\USER32.DLL
-• Получить дескриптор и имя.
-Задаем: FName: G:\WINNT\system32\USER32.DLL
-Получаем:
-Handle: &H77E70000
-Name: User32.dll
-А также будет выполнять последовательно по шагам следующее:
-• используя функцию GetCurrentProcessId определит идентификатор текущего
-процесса;
-• используя функцию GetCurrentProcess определит псевдодескриптор текущего
-процесса;
-• используя функцию DuplicateHandl и значение псевдодескриптора определит
-дескриптор текущего процесса;
-• используя функцию OpenProcess определит копию дескриптора текущего процесса.
-• закроет дескриптор, полученный функцией DuplicateHandl.
-• закроет дескриптор, полученный функцией OpenProcess.
-• выдает список перечисления всех процессов, потоков, модулей и их свойства в
-системе, а также список загруженных драйверов устройств)
-*/
+
 
 #include <iostream>
 #include <string>
@@ -48,10 +12,10 @@ namespace fs = std::filesystem;
 int main()
 {
 
-  HMODULE hModule;  
+  HMODULE hModule;
   char fullName[STRING_LENGTH];
 
-  // Получаем название файла user32.dll с помощью библиотеки filesystem
+  // Получаем название короткое и полное файла lab2 с помощью библиотеки filesystem
   std::string filename = "C:\\Users\\slava\\Desktop\\Project\\!C++\\MOS_LAB\\lab2.exe";
   std::cout << "Process name: " << fs::path(filename).filename() << std::endl;
   std::cout << "Full name of process: " << filename << std::endl;
@@ -93,10 +57,10 @@ int main()
     std::cout << "Opened process is closed" << std::endl;
   else
     std::cout << "Opened process is -NOT- closed" << std::endl;
-
+  
   // Получение всех процессов с помощью библиотеки Toolhelp32
 
-  // Снимок всех процессов
+  // Делаем снимок всех процессов
   HANDLE process_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
   PROCESSENTRY32 process_info;
   process_info.dwSize =  sizeof(PROCESSENTRY32);
@@ -104,6 +68,7 @@ int main()
   std::cout << "********* Start list of processes *********" << std::endl;
   Process32First(process_snapshot, &process_info);
 
+  // Цикл для вывода списка процессов
   do {
     std::cout << "_______Process " << process_info.th32ProcessID << "_______" << std::endl;
     std::cout << "Number of threads: " << process_info.cntThreads << std::endl;
@@ -111,12 +76,12 @@ int main()
     
   }  while (Process32Next(process_snapshot, &process_info));
 
-  CloseHandle(process_snapshot); // закрыли процесс снимка
+  CloseHandle(process_snapshot); // закрываем процесс снимка
   std::cout << "********* End list of processes *********" << std::endl;
 
   // Получение всех потоков с помощью библиотеки Toolhelp32
 
-  // Снимок
+  // Делаем снимок потоков
   HANDLE threads_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
   THREADENTRY32 thread_info;
   thread_info.dwSize = sizeof(THREADENTRY32);
@@ -124,6 +89,7 @@ int main()
   std::cout << "********* Start list of threads *********" << std::endl;
   Thread32First(threads_snapshot, &thread_info);
 
+  // Цикл для вывода списка процессов
   do {
     std::cout << "Thread " << thread_info.th32ThreadID << "_______" 
               << "Owner - " << thread_info.th32OwnerProcessID << std::endl;
@@ -135,7 +101,7 @@ int main()
 
   // Получение всех модулей с помощью библиотеки Toolhelp32
 
-  // Снимок
+  // Делаем снимок модулей
   HANDLE modules_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 0);
   MODULEENTRY32 module_info;
   module_info.dwSize = sizeof(MODULEENTRY32);
@@ -143,6 +109,7 @@ int main()
   std::cout << "********* Start list of modules *********" << std::endl;
   Module32First(modules_snapshot, &module_info);
 
+  // Цикл для вывода списка модулей
   do {
     std::cout << "_____ Module " << module_info.szModule << "_____" << std::endl;
     std::cout << "Path: " << module_info.szExePath << std::endl;
@@ -152,9 +119,11 @@ int main()
     
   }  while (Module32Next(modules_snapshot, &module_info));
 
-  CloseHandle(modules_snapshot); // закрыли процесс снимка
+  // Закрываем процесс снимка
+  CloseHandle(modules_snapshot); 
   std::cout << "********* End list of modules *********" << std::endl;
 
+  // Пауза
   system("pause");
 
   return 0;
